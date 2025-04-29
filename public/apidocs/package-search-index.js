8d99c140a233b9d844bb09783bdee6519f84d335
Jørgen Finsveen <joergen.finsveen@gmail.com>
2025-04-29 10:29:48 +0000

    remove html

--- public/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"no.idata1002.group19"},{"l":"no.idata1002.group19.domain.entity"},{"l":"no.idata1002.group19.domain.repository"},{"l":"no.idata1002.group19.service"},{"l":"no.idata1002.group19.web.api"}];updateSearchResults();
